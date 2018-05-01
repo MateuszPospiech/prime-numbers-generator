0823fe5d99d4abdfd21bd6d092c7baf35ebfe61f
MateuszPospiech <pospiechmateusz@gmail.com>
2018-05-01 12:58:07 +0200

    Update beta version

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,57 @@ const result = document.getElementById('result');
 
 function writeNumber() {
     const numberValue = number.value;
-    result.textContent = numberValue;
+    
+    if(numberValue>2){
+        result.innerHTML = 2;
+    }
+    else if(numberValue<=1){
+        result.innerHTML = 'Liczba pierwsza to liczba NATURALNĄ większa od 1, która ma dokładnie dwa; 1 i samą siebie.';
+    }
+    else{
+        result.innerHTML = '';
+    }
+                      
     
     for(i=1; i <= numberValue; i++){
         
         for(j=2; j <= Math.round(Math.sqrt(i)); j++){
             const pierwiastek = Math.round(Math.sqrt(i));
             const wynik2 = i % j;
-                        
-            console.log(`Liczba j ${j},  Liczba i ${i}, Pierwiastek  ${pierwiastek}, Reszta z dzielenia ${wynik2}`);
+            let wynikii = 0;
+            wynikii = wynikii + wynik2;
             
+            
+              
+            /*console.log(`Liczba j ${j},  Liczba i ${i}, Pierwiastek  ${pierwiastek}, Reszta z dzielenia ${wynik2}`);*/
+            
+            
+            if(wynik2 != 0 && /*wynikii == 1 &&*/ j>= Math.round(Math.sqrt(i)) && numberValue > i ){
+                
+                console.log(`Liczba ${i} jest liczbą pierwszą. Ile obrotów ${wynikii}.`);
+                
+                
+                result.innerHTML += `, ${i}`;
+                
+                
+                break;
+                
             }
+            else if(wynik2 != 0){
+                
+                /*console.log(`Liczba ${i} MOŻE jest LP. Ile obrotów ${wynikii}.`);*/
+            }
+            else{
+                break;
+                console.log(`Liczba ${i} nie jest liczbą pierwszą.  Ile obrotów ${wynikii}`);
+                
+                
+                
+                break;
+                
+            }
+            }
+        
     }
     
 }
